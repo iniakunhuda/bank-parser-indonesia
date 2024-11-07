bac73e2c6c041bfadc661674799fb4b0b6328c32
Mochammad Miftahul Huda Firmansyah <iniakunhuda@gmail.com>
2024-11-07 10:11:16 +0700

    feat: init v0.1

---
+++ README.md
@@ -0,0 +1,77 @@
+# Bank Report Converter
+
+This project is a simple web application that converts bank statements into CSV format. It supports various bank formats and allows users to select the type of statement, upload the PDF, and download the parsed transactions in CSV format.
+
+## Features
+
+-   Parses bank statements from:
+    -   **BCA Debit Statements**
+    -   **Mandiri Debit Statements**
+    -   **Mandiri Credit Card Statements**
+-   Converts parsed data into CSV format for easy download.
+-   Provides user prompts for password-protected PDF statements.
+-   Automatically detects and formats certain transaction descriptions (e.g., for popular services like GoPay, ShopeePay).
+
+## Technologies Used
+
+-   **React** with TypeScript for the UI and state management.
+-   **Mantine UI** for UI components (button, table, etc.).
+-   **@fahmifan/bank-statement-parser-id** for bank statement parsing.
+-   **json2csv** for converting JSON data into CSV format.
+-   **@tabler/icons-react** for icons.
+
+## Getting Started
+
+### Prerequisites
+
+-   **Node.js** and **npm** installed on your machine.
+
+### Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/your-username/bank-report-converter.git
+    cd bank-report-converter
+    ```
+
+2. Install dependencies:
+    ```bash
+    npm install
+    ```
+
+### Running the Application
+
+To start the application, run:
+
+```bash
+npm start
+```
+
+The app will be available at http://localhost:3000.
+
+### Building for Production
+
+To build the app for production, run:
+
+```bash
+npm run build
+```
+
+The built files will be in the build directory.
+
+## Usage
+
+1. Select the type of bank statement from the dropdown menu (e.g., BCA Debit Statement, Mandiri Debit Statement).
+2. Click on "Upload Bank Report" to upload your bank statement PDF.
+3. If the PDF is password-protected, you’ll be prompted to enter the password.
+4. After the statement is parsed, a table will display the transaction details.
+5. Click "Download CSV" to export the transactions as a CSV file.
+
+## Contributing
+
+Contributions are welcome! Please create a pull request with a detailed description of your changes.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ src/App.tsx
@@ -0,0 +1,263 @@
+import {
+	Button,
+	Container,
+	FileInput,
+	Group,
+	Select,
+	Table,
+	Title,
+} from "@mantine/core";
+import { useEffect, useState } from "react";
+import { parseBCAStatement, parseMandiriCCStatement, parseMandiriStatement, TrxRecord } from "@fahmifan/bank-statement-parser-id";
+
+import { IconDownload, IconUpload } from "@tabler/icons-react";
+import { json2csv } from "json-2-csv";
+
+type ParserKind = "bca_statement" | "mandiri_statement" | "mandiri_cc_statement";
+
+export function App() {
+	const [file, setFile] = useState<File | null>(null);
+	const [trxRecords, setTrxRecords] = useState<TrxRecord[]>([]);
+	const [parserKind, setParserKind] = useState<ParserKind>("bca_statement");
+
+	const parserKinds: {
+		value: ParserKind;
+		label: string;
+	}[] = [
+			{ value: "bca_statement", label: "BCA Debit Statement" },
+			{ value: "mandiri_statement", label: "Mandiri Debit Statement" },
+			{ value: "mandiri_cc_statement", label: "Mandiri CC Statement" }
+		];
+
+	useEffect(() => {
+		convertFiles();
+	}, [file, parserKind]);
+
+	function convertFiles() {
+		if (!file) {
+			return;
+		}
+
+		file
+			?.arrayBuffer()
+			.then((buf) => {
+				return buf;
+			})
+			.then((buf) => {
+				switch (parserKind) {
+					case "bca_statement":
+						return parseBCAStatement(
+							buf,
+							askPasswordPrompt,
+							askRetryPasswordPrompt,
+						);
+					case "mandiri_statement":
+						return parseMandiriStatement(
+							buf,
+							askPasswordPrompt,
+							askRetryPasswordPrompt,
+						);
+					case "mandiri_cc_statement":
+						return parseMandiriCCStatement(
+							buf,
+							askPasswordPrompt,
+							askRetryPasswordPrompt,
+						)
+					default:
+						return [];
+				}
+			})
+			.then((res) => {
+				setTrxRecords(res);
+			});
+	}
+
+	function onChange(value: File | null) {
+		setFile(value);
+	}
+
+	function askPasswordPrompt(): string {
+		const pass = window.prompt(
+			"The PDF is password protected. Please enter password to continue.",
+		);
+		return pass || "";
+	}
+
+	function askRetryPasswordPrompt(): string {
+		const pass = window.prompt(
+			"The password is incorrect. Please enter the correct password to continue.",
+		);
+		return pass || "";
+	}
+
+	function exportToCSV() {
+		const records = trxRecords.map((rec) => {
+			return {
+				tgl: `${rec.tgl}/${rec.tahun}`,
+				ket: rec.ket,
+				type: rec.type,
+				mutasi: rec.mutasi,
+				saldo: rec.saldo,
+			};
+		});
+		json2csv(records, {
+			delimiter: {
+				field: ",",
+				wrap: '"',
+				eol: "\n",
+			},
+		}).then((res) => {
+			downloadCSV(
+				res,
+				replaceExtenstion(file?.name ?? "bank_statement", "csv"),
+				"text/csv",
+			);
+		});
+	}
+
+	function replaceExtenstion(input: string, newExt: string): string {
+		const parts = input.split(".");
+		parts[parts.length - 1] = newExt;
+		return parts.join(".");
+	}
+
+	return (
+		<>
+			<Container size="md">
+				<Title>Bank Report Converter</Title>
+				<Group py="md">
+					<Select
+						placeholder="Select Report Type"
+						value={parserKind}
+						onChange={(val) => {
+							setParserKind(val as ParserKind);
+						}}
+						data={parserKinds}
+					/>
+					<FileInput
+						value={file}
+						icon={<IconUpload size={14} />}
+						onChange={onChange}
+						placeholder="Upload Bank Report"
+					/>
+					<Button
+						variant="outline"
+						leftIcon={<IconDownload size={14} />}
+						onClick={() => {
+							exportToCSV();
+						}}
+					>
+						Download CSV
+					</Button>
+				</Group>
+
+				{trxRecords.length > 0 && (
+					<Table highlightOnHover>
+						<thead>
+							<tr>
+								<th>Tanggal</th>
+								<th>Keterangan</th>
+								<th>Mutasi</th>
+								{/* <th>DB/CR</th> */}
+							</tr>
+						</thead>
+						<tbody>
+							{trxRecords.map((trxRecord, index) => {
+								return (
+									<tr
+										key={trxRecord.ket + trxRecord.tgl + trxRecord.type + index}
+									>
+										<td>
+											{trxRecord.tgl}/{trxRecord.tahun}
+										</td>
+										<td>{formatKeterangan(trxRecord.ket)}</td>
+										<td style={{ color: getRecordColor(trxRecord) }}>
+											{getMutationText(trxRecord)}
+										</td>
+										{/* <td>{trxRecord.type}</td> */}
+									</tr>
+								);
+							})}
+						</tbody>
+					</Table>
+				)}
+			</Container>
+		</>
+	);
+}
+
+function rupiah(number: number): string {
+	return new Intl.NumberFormat("id-ID", {
+		style: "currency",
+		currency: "IDR",
+	}).format(number);
+}
+
+function getMutationText(record: TrxRecord): string {
+	switch (record.type) {
+		case "credit":
+		case "saldo_awal":
+			return `+${rupiah(record.mutasi)}`;
+		default:
+			return `-${rupiah(record.mutasi)}`;
+	}
+}
+
+function getRecordColor(record: TrxRecord) {
+	switch (record.type) {
+		case "credit":
+		case "saldo_awal":
+			return "#2F9E44";
+		case "debit":
+			return "#FF0000";
+		default:
+			return "";
+	}
+}
+
+function formatKeterangan(keterangan: string) {
+	if (keterangan.includes("UBP60146073701FFFFFF")) {
+		return keterangan + " **(GOJEK)**";
+	} else if (keterangan.includes("UBP60148930801")) {
+		return keterangan + " **(SHOPEEPAY)**";
+	} else if (keterangan.includes("UBP6014530180")) {
+		return keterangan + " **(PEMBAYARAN KAI)**";
+	} else if (keterangan.includes("UBP6014400190")) {
+		return keterangan + " **(MANDIRI PULSA)**";
+	} else if (keterangan.includes("UBP60146000101FF")) {
+		return keterangan + " **(GRAB / OVO)**";
+	} else if (keterangan.includes("UBP60148890801F")) {
+		return keterangan + " **(MIDTRANS)**";
+	} else if (keterangan.includes("UBP6014603290")) {
+		return keterangan + " **(E-MONEY)**";
+	}
+
+	return keterangan;
+}
+
+// downloadCSV takes a CSV string, the filename and mimeType as parameters
+function downloadCSV(
+	csvContent: string,
+	fileName: string,
+	fileMimeType: string,
+) {
+	const a = document.createElement("a");
+	const mimeType = fileMimeType || "application/octet-stream";
+
+	if (window.URL && "download" in a) {
+		//html5 A[download]
+		a.href = window.URL.createObjectURL(
+			new Blob([csvContent], {
+				type: mimeType,
+			}),
+		);
+		a.setAttribute("download", fileName);
+		document.body.appendChild(a);
+		a.click();
+		document.body.removeChild(a);
+	} else {
+		location.href = `data:application/octet-stream, ${encodeURIComponent(
+			csvContent,
+		)}`; // only this mime type is supported
+	}
+}
